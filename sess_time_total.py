90544d9b9b36e082740e1445ddee1972c566a1bf
brunobonil <brunobonil@gmail.com>
2021-06-16 19:18:51 -0300

    Funcion 3 Terminada

--- sess_time_total.py
+++ sess_time_total.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import datetime as dt
 
+
 # Pasar inicio y fin como una tupla (año, mes, dia)
 def session_time_total(datos, inicio, fin):
-    inicio = dt.date(inicio)
-    fin = dt.date(fin)
+    inicio = dt.date(int(inicio[0]), int(inicio[1]), int(inicio[2]))
+    fin = dt.date(fin[0], fin[1], fin[2])
     aux = list()
     tot_sess = 0
     for i in datos:
