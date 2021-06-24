ee92f0750b71280d4ff29ce6416cbee7c0f561fa
MarianoSaez <mm.saez@alumno.um.edu.ar>
2021-06-23 22:41:37 -0300

    update

---
+++ main.py
@@ -0,0 +1,3 @@
+"""
+Unimos todo
+"""
--- sess_time_total.py
+++ sess_time_total.py
@@ -1,8 +1,7 @@
 # Seguimiento del tiempo de sesión (Session Time) de un usuario en un período de tiempo establecido (rango de fechas). 
 # Debe incluir una lista de usuarios y la posibilidad de ingresar un rango de fechas.
-
-
-'''['mleenw', Timestamp('2019-08-29 08:40:00'), datetime.datetime(2019, 8, 29, 8, 41), 23.0]
+'''
+['mleenw', Timestamp('2019-08-29 08:40:00'), datetime.datetime(2019, 8, 29, 8, 41), 23.0]
 ['mleenw', Timestamp('2019-08-29 08:41:00'), datetime.datetime(2019, 8, 29, 8, 45), 244.0]
 ['mleenw', Timestamp('2019-08-29 09:07:00'), datetime.datetime(2019, 8, 29, 9, 12), 343.0]
 ['mleenw', Timestamp('2019-08-29 10:03:00'), datetime.datetime(2019, 8, 29, 14, 1), 14267.0]
@@ -13,24 +12,29 @@
 ['mleenw', Timestamp('2019-08-30 08:58:00'), datetime.datetime(2019, 8, 30, 9, 7), 560.0]
 ['mleenw', Timestamp('2019-08-30 09:09:00'), datetime.datetime(2019, 8, 30, 9, 14), 274.0]
 ['mleenw', Timestamp('2019-08-30 09:14:00'), datetime.datetime(2019, 8, 30, 9, 24), 566.0]
-['mleenw', Timestamp('2019-08-30 10:24:00'), datetime.datetime(2019, 8, 30, 14, 2), 13101.0]'''
-
+['mleenw', Timestamp('2019-08-30 10:24:00'), datetime.datetime(2019, 8, 30, 14, 2), 13101.0]
+'''
 import pandas as pd
 import funcion2 as f2
+import datetime as dt
 
 
 def session_time_total(datos, inicio, fin):
-    aux = list()
+    date_inicio = dt.datetime(2019, 8, 29).date()
+    date_final = dt.datetime(2019, 8, 30).date()
+    sess_time_total = 0
     for i in datos:
-        if inicio in i[1] or fin in i[1]:
-            aux.append(i)
-        else:
-            break
-    print(aux)
+        try:
+            if pd.to_datetime(i[1]).date() >= date_inicio and pd.to_datetime(i[2]).date() <= date_final:
+                sess_time_total += i[3]
+        except:
+            continue
+    return sess_time_total
+
 
 if __name__ == '__main__':
     datos = f2.tiempos_usuario('mleenw')
-    session_time_total(datos, '2019-08-29', '2019-08-30')
-    print(datos[0][2])
+    rango = session_time_total(datos, '2019-08-29', '2019-08-30')
+    print(rango)
 
 
